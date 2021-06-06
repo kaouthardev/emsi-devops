71ce1ee533ecd91de6c9ab4d2c8e542d0c5b9606
Souhail KARAM <karam.souhail@systematis.ma>
2021-06-06 19:42:52 +0100

    API Advanced

--- 0x06-api_advanced/0-subs.py
+++ 0x06-api_advanced/0-subs.py
@@ -2,10 +2,10 @@
 
 import requests
 
-def number_of_subscribers(subreddit)
+def number_of_subscribers(subreddit):
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     req = requests.get(url, headers={'User-Agent': 'Kaouthardev'}, allow_redirects=False)
 
-    if req.status_code != 200
+    if req.status_code != 200:
         return 0
-    return req.json().get('data').get('subscribers')
+    return req.json()['data']['subscribers']
